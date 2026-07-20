7bc72f02b192430e2e90fbc3f7d1b52c5c74b91b
Umirzakovv <muhammadyosinumirzakov601@gmail.com>
2026-07-20 15:15:46 +0500

    s

--- src/app/providers/ui/AppProvider.tsx
+++ src/app/providers/ui/AppProvider.tsx
@@ -1,9 +1,34 @@
-import type { PropsWithChildren } from 'react'
+import { useEffect, type PropsWithChildren } from 'react'
 import { AntProvider } from '../ant-provider'
 import { PostHogProvider } from '../posthog-provider'
 import { StoreProvider } from '../store-provider'
 
+let isTelegramWebAppInitialized = false
+
+function initTelegramWebApp() {
+  if (isTelegramWebAppInitialized) {
+    return
+  }
+
+  const telegramWebApp = window.Telegram?.WebApp
+
+  if (!telegramWebApp) {
+    return
+  }
+
+  isTelegramWebAppInitialized = true
+
+  telegramWebApp.ready()
+  telegramWebApp.expand?.()
+  telegramWebApp.requestFullscreen?.()
+  telegramWebApp.disableVerticalSwipes?.()
+}
+
 export function AppProvider({ children }: PropsWithChildren) {
+  useEffect(() => {
+    initTelegramWebApp()
+  }, [])
+
   return (
     <PostHogProvider>
       <StoreProvider>
---
+++ src/shared/types/telegram-web-app.d.ts
@@ -0,0 +1,14 @@
+export {}
+
+declare global {
+  interface Window {
+    Telegram?: {
+      WebApp?: {
+        ready: () => void
+        expand?: () => void
+        requestFullscreen?: () => void
+        disableVerticalSwipes?: () => void
+      }
+    }
+  }
+}
